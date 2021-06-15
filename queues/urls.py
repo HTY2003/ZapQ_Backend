38d9036a8c7c15108e2d0c849acebd330f1ef34e
HTY2003 <randumbperson@gmail.com>
2021-06-15 23:36:12 +0800

    NO BONNNERRRRRR

--- queues/urls.py
+++ queues/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 
 urlpatterns = [
     path('business/create_queue/', QueueMake.as_view()),
     path('business/my_queue/', QueueMaker.as_view()),
     path('business/advance_queue/', QueueNext.as_view()),
-    path('business/end_queue/', QueueDel.as_view()),
+    path('business/end_queue/', QueueEnd.as_view()),
     path('business/pause_queue/', QueuePause.as_view()),
 
     path('user/join_queue/', UserJoinQueue.as_view()),
@@ -15,7 +14,6 @@ urlpatterns = [
     path('user/get_queue/', UserInQueue.as_view()),
     path('user/get_all_queue_ids/', UserAllQueues.as_view()),
 
-    path('signup/', create_account.as_view()),
-    path('login/', login.as_view()),
-    path('logout/', logout.as_view()),
+    path('signup/', UserAdd.as_view()),
+    path('login/', UserLogin.as_view()),
 ]
